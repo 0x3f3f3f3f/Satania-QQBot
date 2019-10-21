26a1845c4b06379503948063c8ac88166b214eac
大fa师 <chfreezer00@gmail.com>
2019-10-21 20:13:57 +0800

    从酷Q移植到QQLight作为宿主

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const WebSocket = require('ws');
 const request = require('request');
 const _ = require('lodash');
 const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid/v1');
 
 // 账号密码
 const secret = JSON.parse(fs.readFileSync('./secret.json', 'utf8'));
 
-const client = new WebSocket(`ws://${secret.coolqHost}:${secret.coolqPort}/?access_token=${secret.coolqAccessToken}`);
+// 复读机字典
+const repeaterDic = {};
+
+const client = new WebSocket(`ws://${secret.coolqHost}:${secret.coolqPort}/`);
 
 client.on('open', () => {
     console.log('opend!');
@@ -24,66 +29,70 @@ client.on('message', data => {
     if (!_.isString(data)) return;
     // console.log('=>', data);
 
-    let receiveObj;
+    let recvObj;
     try {
-        receiveObj = JSON.parse(data);
+        recvObj = JSON.parse(data);
     } catch {}
-    if (_.isEmpty(receiveObj)) return;
+    if (_.isEmpty(recvObj)) return;
 
-    // log返回值
-    if (receiveObj.status && receiveObj.retcode)
-        console.log('return:', receiveObj);
+    // if (saucenaoDic[recvObj.id]) {
+    //     sendSauceNAO(recvObj.id, recvObj.result);
+    //     return;
+    // }
 
-    if (!receiveObj.message) return;
+    if (recvObj.event != 'message') {
+        console.log('=>', recvObj);
+        return;
+    }
 
-    console.log('msg:', receiveObj.message);
+    console.log('content:', recvObj.params.content);
 
     let atMe = false;
-    if ((new RegExp('CQ:at')).test(receiveObj.message) &&
-        (new RegExp(`qq=${secret.targetQQ}`)).test(receiveObj.message)) {
+    if ((new RegExp('QQ:at')).test(recvObj.params.content) &&
+        (new RegExp(secret.targetQQ)).test(recvObj.params.content)) {
         atMe = true;
     }
 
     // 被at了
     if (atMe) {
         // 搜图功能
-        if ((/搜.*图/).test(receiveObj.message)) {
-            const imgURL = getFirstImageURL(receiveObj.message);
+        if ((/搜.*图/).test(recvObj.params.content)) {
+            const imgURL = getFirstImageURL(recvObj.params.content);
             if (!imgURL) {
                 client.send(JSON.stringify({
-                    action: 'send_msg',
+                    id: uuid(),
+                    method: "sendMessage",
                     params: {
-                        message_type: receiveObj.message_type || null,
-                        user_id: receiveObj.user_id || null,
-                        group_id: receiveObj.group_id || null,
-                        discuss_id: receiveObj.discuss_id || null,
-                        message: '欧尼酱搜图的话请至少要一张图哦~'
+                        type: recvObj.params.type,
+                        group: recvObj.params.group || '',
+                        qq: recvObj.params.qq || '',
+                        content: '欧尼酱搜图的话请至少要一张图哦~'
                     }
                 }));
             } else {
-                SauceNAO(imgURL);
+                searchSauceNAO(imgURL);
             }
         } else {
             client.send(JSON.stringify({
-                action: 'send_msg',
+                id: uuid(),
+                method: "sendMessage",
                 params: {
-                    message_type: receiveObj.message_type || null,
-                    user_id: receiveObj.user_id || null,
-                    group_id: receiveObj.group_id || null,
-                    discuss_id: receiveObj.discuss_id || null,
-                    message: '欧尼酱~想我了吗？'
+                    type: recvObj.params.type,
+                    group: recvObj.params.group || '',
+                    qq: recvObj.params.qq || '',
+                    content: '欧尼酱~想我了吗？'
                 }
             }));
         }
     }
 
-    async function SauceNAO(url) {
+    async function searchSauceNAO(url) {
         let saucenaoObj;
         try {
             saucenaoObj = await new Promise((resolve, reject) => {
                 request.get(encodeURI(`https://saucenao.com/search.php?db=999&output_type=2&numres=1&api_key=${secret.SauceNAO_API_KEY}&url=${url}`),
-                    (error, response, body) => {
-                        if (error) {
+                    (err, res, body) => {
+                        if (err) {
                             reject();
                             return;
                         }
@@ -101,100 +110,81 @@ client.on('message', data => {
             });
         } catch {
             client.send(JSON.stringify({
-                action: 'send_msg',
+                id: uuid(),
+                method: "sendMessage",
                 params: {
-                    message_type: receiveObj.message_type || null,
-                    user_id: receiveObj.user_id || null,
-                    group_id: receiveObj.group_id || null,
-                    discuss_id: receiveObj.discuss_id || null,
-                    message: '欧尼酱搜索出错了~喵'
+                    type: recvObj.params.type,
+                    group: recvObj.params.group || '',
+                    qq: recvObj.params.qq || '',
+                    content: '欧尼酱搜索出错了~喵'
                 }
             }));
             return;
         }
         if (!saucenaoObj.results) {
             client.send(JSON.stringify({
-                action: 'send_msg',
+                id: uuid(),
+                method: "sendMessage",
                 params: {
-                    message_type: receiveObj.message_type || null,
-                    user_id: receiveObj.user_id || null,
-                    group_id: receiveObj.group_id || null,
-                    discuss_id: receiveObj.discuss_id || null,
-                    message: '欧尼酱对不起，没有找到你要的~'
+                    type: recvObj.params.type,
+                    group: recvObj.params.group || '',
+                    qq: recvObj.params.qq || '',
+                    content: '欧尼酱对不起，没有找到你要的~'
                 }
             }));
             return;
         }
+
         client.send(JSON.stringify({
-            action: 'send_msg',
+            id: uuid(),
+            method: "sendMessage",
             params: {
-                message_type: receiveObj.message_type || null,
-                user_id: receiveObj.user_id || null,
-                group_id: receiveObj.group_id || null,
-                discuss_id: receiveObj.discuss_id || null,
-                message: [{
-                        type: 'at',
-                        data: {
-                            qq: receiveObj.user_id
-                        }
-                    },
-                    {
-                        type: 'text',
-                        data: {
-                            text: ' 欧尼酱是不是你想要的内个~\r\n' +
-                                `相似度：${saucenaoObj.results[0].header.similarity}%\r\n` +
-                                ((saucenaoObj.results[0].data.title ||
-                                    saucenaoObj.results[0].data.jp_name ||
-                                    saucenaoObj.results[0].data.eng_name) ? `标题：${
-                                        saucenaoObj.results[0].data.title||
-                                        saucenaoObj.results[0].data.jp_name||
-                                        saucenaoObj.results[0].data.eng_name}\r\n` : '') +
-                                ((saucenaoObj.results[0].data.member_name ||
-                                    saucenaoObj.results[0].data.author_name ||
-                                    saucenaoObj.results[0].data.creator) ? `作者：${
-                                        saucenaoObj.results[0].data.member_name||
-                                        saucenaoObj.results[0].data.author_name||
-                                        saucenaoObj.results[0].data.creator}\r\n` : '')
-                        }
-                    },
-                    {
-                        type: 'image',
-                        data: {
-                            file: saucenaoObj.results[0].header.thumbnail
-                        }
-                    },
-                    {
-                        type: 'text',
-                        data: {
-                            text: (saucenaoObj.results[0].data.ext_urls ? ('\r\n' + saucenaoObj.results[0].data.ext_urls[0]) : '')
-                        }
-                    }
-                ]
+                type: recvObj.params.type,
+                group: recvObj.params.group || '',
+                qq: recvObj.params.qq || '',
+                content: `[QQ:at=${recvObj.params.qq}]` +
+                    ' 欧尼酱是不是你想要的内个~\r\n' +
+                    `相似度：${saucenaoObj.results[0].header.similarity}%\r\n` +
+                    ((saucenaoObj.results[0].data.title ||
+                        saucenaoObj.results[0].data.jp_name ||
+                        saucenaoObj.results[0].data.eng_name) ? `标题：${
+                        saucenaoObj.results[0].data.title||
+                        saucenaoObj.results[0].data.jp_name||
+                        saucenaoObj.results[0].data.eng_name}\r\n` : '') +
+                    ((saucenaoObj.results[0].data.member_name ||
+                        saucenaoObj.results[0].data.author_name ||
+                        saucenaoObj.results[0].data.creator) ? `作者：${
+                        saucenaoObj.results[0].data.member_name||
+                        saucenaoObj.results[0].data.author_name||
+                        saucenaoObj.results[0].data.creator}\r\n` : '') +
+                    `[QQ:pic=${saucenaoObj.results[0].header.thumbnail}]` +
+                    (saucenaoObj.results[0].data.ext_urls ? ('\r\n' + saucenaoObj.results[0].data.ext_urls[0]) : '')
             }
         }));
     }
 
     // 启动复读机模式
     if (!repeaterMode &&
-        lastMassage == receiveObj.message) {
+        repeaterDic[recvObj.params.group] == recvObj.params.content) {
         repeaterMode = true;
+
         client.send(JSON.stringify({
-            action: 'send_msg',
+            id: uuid(),
+            method: "sendMessage",
             params: {
-                message_type: receiveObj.message_type || null,
-                user_id: receiveObj.user_id || null,
-                group_id: receiveObj.group_id || null,
-                discuss_id: receiveObj.discuss_id || null,
-                message: receiveObj.message
+                type: recvObj.params.type,
+                group: recvObj.params.group || '',
+                qq: recvObj.params.qq || '',
+                content: recvObj.params.content
             }
         }));
     } else if (repeaterMode &&
-        lastMassage != receiveObj.message) {
+        repeaterDic[recvObj.params.group] != recvObj.params.content) {
         repeaterMode = false;
     }
 
     // 记录最后一条聊天信息
-    lastMassage = receiveObj.message;
+    repeaterDic[recvObj.params.group] = recvObj.params.content;
 });
 
 client.on('pong', () => {
@@ -205,13 +195,16 @@ client.on('close', (code, reason) => {
     console.log('closed:', code, reason);
 });
 
-function getFirstImageURL(message) {
-    const cqArray = message.match(/\[.*?\]/g);
-    if (!_.isArray(cqArray)) return null;
+function getFirstImageURL(content) {
+    const arr = content.match(/\[.*?\]/g);
+    if (!_.isArray(arr)) return null;
 
-    for (let i = 0; i < cqArray.length; i++) {
-        const cq = cqArray[i].replace(/\[|\]/g, '').split(',');
-        if (cq[0] == 'CQ:image')
-            return cq[2].replace('url=', '');
+    for (let i = 0; i < arr.length; i++) {
+        const qq = arr[i].replace(/\[|\]/g, '').split('=');
+        if (qq[0] == 'QQ:pic') {
+            const iniPath = path.join(secret.tempPath, 'image', path.basename(qq[1], path.extname(qq[1])) + '.ini');
+            const ini = fs.readFileSync(iniPath, 'utf8');
+            return /^url=(.*)/m.exec(ini)[1].trim();
+        }
     }
 }

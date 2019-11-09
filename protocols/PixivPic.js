70680311aee85f3033652c6ea65f5964011064fd
大fa师 <chfreezer00@gmail.com>
2019-11-09 23:03:37 +0800

    排名过后的不出现在随机色图里

--- protocols/PixivPic.js
+++ protocols/PixivPic.js
@@ -104,7 +104,7 @@ function testIllust(Illust) {
 async function setuDownload(regExp = null) {
     if (setuPool.length == 0) return null;
 
-    let setuIndex = parseInt(Math.random() * setuPool.length);
+    let setuIndex = parseInt(Math.random() * Math.min(50, setuPool.length));
 
     if (regExp) {
         let isHit = false;

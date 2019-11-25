92451a7c20ed17b4f089ce598ff3a667f3118c1d
大fa师 <chfreezer00@gmail.com>
2019-11-25 23:12:13 +0800

    完善支持ajax跨域

--- Pixiv_web_service.js
+++ Pixiv_web_service.js
@@ -69,6 +69,7 @@ app.use(express.json());
 // 支持跨域
 app.all('*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
